70c17e8a558323a811091c31067fd6f53f0aa8b5
abythomas300 <abythomas300@gmail.com>
2026-01-31 23:07:13 +0530

    algorithm: selection sort with 100k test input and execution time log

--- Algorithms/selection_sort.js
+++ Algorithms/selection_sort.js
@@ -20,18 +20,31 @@ function smallestValue(data) {
 
 // main
 function selectionSort(data) {
+    console.log("selection sort function execution started...")
+    const startTime = performance.now()
     let testDataCopy = [...data] // keep copy of original data and operate on it
     while(testDataCopy.length > 0) { 
         let smIndex = smallestValue(testDataCopy)
         result.push(testDataCopy[smIndex])
         testDataCopy.splice(smIndex, 1)
     }
+    const endTime = performance.now()
+    return endTime - startTime
 }
 
-const testData = [66687, 0.89, 1998, -2, 0.0053]
+const testData = []
 let result = []
 
-selectionSort(testData)
+for(let i=1; i<=100000; i++) {
+    testData.push(i)
+}
+console.log("testData: ", testData)
+const timeTaken = selectionSort(testData)
 
 console.log("Unsorted Data:", testData)
 console.log("Sorted Data:", result)
+console.log(`Completed in ${timeTaken/1000}s`)
+
+// Algorithmic Complexity
+// Time: O(n^2)
+// Space: O(1)
